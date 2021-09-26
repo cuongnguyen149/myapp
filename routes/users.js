4c9a5ecaaa74325a3c74b35f6a3a19bf04f31c44
Cuong <cuongnguyen149@gmail.com>
2021-09-26 15:35:47 +0700

    parse number

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const add = require('../services/number');
+const number = require('../services/number');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   const { a = 0, b = 0 } = req.query;
-  const total = add.add(a, b);
+  const total = number.add(+a, +b);
   res.send({ total });
 });
 
