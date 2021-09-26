547b41a177ff1e0cddc4e2c22bf0b07094c332d0
Cuong <cuongnguyen149@gmail.com>
2021-09-26 15:33:54 +0700

    change params to query

--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,9 @@ const add = require('../services/number');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
+  const { a = 0, b = 0 } = req.query;
   const total = add.add(a, b);
+  res.send({ total });
+});
+
 module.exports = router;
