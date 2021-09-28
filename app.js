06d0ecfdcca464e7855b6d30c534e417b2bb6808
Cuong <cuongnguyen149@gmail.com>
2021-09-28 20:58:02 +0700

    use helmet

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var helmet = require('helmet');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var calculationsRouter = require('./routes/calculations');
-
 var app = express();
 
 // view engine setup
@@ -18,17 +18,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet.hidePoweredBy());
 
 app.use('/', indexRouter);
 app.use('/calculations', calculationsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
